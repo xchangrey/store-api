859c82992c9117aed339c1de8afa9d06bdd9df72
Christian Geroy <cr50.christian@enfrausaph.com>
2018-08-10 17:49:02 -0400

    Adding the POST, GET and PUT methods for the products and wishlists

--- server.js
+++ server.js
@@ -20,6 +20,38 @@ app.post('/product', function(req,res){
   });
 });
 
+app.post('/wishlist', function(req,res){
+  var wishlist = new Wishlist();
+  wishlist.title = req.body.title;
+  wishlist.save(function(err, newWishlist){
+    err ? res.status(500).send({error:`Could not save wishlist`}) : res.send(newWishlist);
+  });
+});
+
+app.put('/wishlist/product/add', function(req,res){
+  Product.findOne({_id: req.body.productId}, function(err, product){
+    err ? res.status(500).send({error:`Could not add item to wishlist`}) : Wishlist.update({_id:req.body.wishlistId}, {$addToSet:{products:product._id}}, function(err, wishlist){
+      err ? res.status(500).send({error:`Could not add item to wishlist`}) : res.send(`Successfully added the wishlist`);
+    });
+  });
+});
+
+app.get('/product', function(req,res){
+  Product.find({}, function(err, products){
+    err ? res.status(500).send({error:`Could not retrieve products`}) : res.send(products);
+  })
+});
+
+app.get('/wishlist', function(req,res){
+  Wishlist.find({})
+  .populate({path:'products', model: 'Product'})
+  .exec(function(err,wishlists){
+    err ? res.status(500).send({error:`Could not fetch wishlist`}) : res.send(wishlists);
+  });
+});
+
+
+
 app.listen(port, () => console.log(`Store API Server running on port ${port}`));
 
 
